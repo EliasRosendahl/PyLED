280ef908a819f142ce19ca3db2a62d8967a9ede3
eliasrosendahl <jensen.eliasrosendahl@gmail.com>
2018-10-30 19:27:28 +0100

    app now recieves led status from user interface

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+
+from . import setLED
+
 app = Flask(__name__)
 
 socketio = SocketIO(app)
@@ -7,11 +10,12 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
+    print("test")
     return render_template("index.html")
 
 @socketio.on('setLED')
 def setLED(data):
-    pass
+    print(data)
 
 
 if __name__ == "__main__":
--- static/socketHandler.js
+++ static/socketHandler.js
@@ -1,17 +1,30 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 
 var check1 = document.querySelector("input[name=LED1]");
 var check2 = document.querySelector("input[name=LED2]");
 var check3 = document.querySelector("input[name=LED3]");
 
+function getData(){
+    var data = [];
+    data.push(check1.checked);
+    data.push(check2.checked);
+    data.push(check3.checked);
+    return data;
+}
+
 check1.addEventListener('change', function(){
     console.log("LED1 set to " + this.checked);
+    socket.emit('setLED', getData());
 });
 
 check2.addEventListener('change', function(){
     console.log("LED2 set to " + this.checked);
+    socket.emit('setLED', getData());
+
 });
 
 check3.addEventListener('change', function(){
     console.log("LED3 set to " + this.checked);
+    socket.emit('setLED', getData());
 });

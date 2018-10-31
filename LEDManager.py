bad55a07aa888333e86d0b435928d3f4250a0f38
eliasrosendahl <jensen.eliasrosendahl@gmail.com>
2018-10-31 22:24:10 +0100

    adds error handling for LEDmanager

--- LEDManager.py
+++ LEDManager.py
@@ -3,13 +3,17 @@ import serial
 
 class LEDManager(object):
     def __init__(self):
-        self.ser = serial.Serial('COM8', 9600)
+        try:
+            self.ser = serial.Serial('COM8', 9600)
+        except:
+            print("could not establish connection")
 
     def setLED(self, LED1, LED2, LED3):
 
-        #Turns the 3 args into a single byte
-        c = LED1 + LED2*2 + LED3*4
-        b = bytes([c])
+        if self.ser is not None:
+            #Turns the 3 args into a single byte
+            c = LED1 + LED2*2 + LED3*4
+            b = bytes([c])
 
-        #Writes b to the COM port
-        self.ser.write(b)
+            #Writes b to the COM port
+            self.ser.write(b)
